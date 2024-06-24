ae0d1eabda48ab80c98e9eb5763b1155b41963df
Steven <ssebexen@gmail.com>
2024-06-24 14:28:32 -0400

    Made array display prettier.

--- src/component/AlgorithmResultVisualizer.tsx
+++ src/component/AlgorithmResultVisualizer.tsx
@@ -6,13 +6,24 @@ interface AlgorithmResultVisualizerProps<T> {
 
 export function AlgorithmResultVisualizer<T>(props: AlgorithmResultVisualizerProps<T>) {
   const [stepsVisible, setStepsVisible] = useState<boolean>(false);
+
+  function displayResult(result: T): JSX.Element {
+    return (
+      result instanceof Array
+        ? <div className='flex'>
+            {result.map(ele => <p className='flex-none basis-20 px-2 border text-center'>{String(ele)}</p>)}
+          </div>
+        : <p>{String(result)}</p>
+    );
+  }
+
   return (
     <>
       <p>Result</p>
-      <p>{String(props.result.result)}</p>
+      {displayResult(props.result.result)}
       <p className='select-none' onClick={() => setStepsVisible(!stepsVisible)}>{stepsVisible ? 'v Hide' : '> Show'} steps</p>
       {stepsVisible &&
-        props.result.steps.map((step, index) => <p key={index}>{String(step)}</p>)
+        props.result.steps.map((step, index) => displayResult(step))
       }
     </>
   );
