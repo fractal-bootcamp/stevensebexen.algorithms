f08ae522ee073d2dfe40c5f947d214299e224440
Steven <ssebexen@gmail.com>
2024-06-24 09:55:39 -0400

    Initial

---
+++ src/app/sort/bubble/page.tsx
@@ -0,0 +1,7 @@
+export default function BubbleSort() {
+  return (
+    <>
+      Hello sus!
+    </>
+  )
+}
---
+++ src/app/sort/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Sort() {
+  return (
+    <>
+      <Link href='/sort/bubble'>Bubble</Link>
+    </>
+  )
+}
