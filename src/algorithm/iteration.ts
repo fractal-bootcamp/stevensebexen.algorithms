ee4091d1b7895a666fb699a9f1f8aa70065926d5
Steven <ssebexen@gmail.com>
2024-06-24 10:56:46 -0400

    Rewrote iteration functionality for readability.

--- src/algorithm/iteration.ts
+++ src/algorithm/iteration.ts
@@ -1,14 +1,23 @@
-export function iterateUntilDone<T>(fn: (x: T) => IterationStepResult<T>, inputValue: T, steps: IterationSteps<T> = [], depth = 10000): IterationSteps<T> {
+// Creates an array of intermediate results from running fn on inputValue.
+export function iterationSteps<T>(fn: (x: T) => IterationStepResult<T>, inputValue: T, steps: IterationSteps<T> = [], depth = 10000): IterationSteps<T> {
   if (depth <= 0) throw new Error('Recursion depth max reached.');
 
   const stepResult: IterationStepResult<T> = fn(inputValue);
-  const iterationSteps: IterationSteps<T> = [...steps, stepResult.result];
+  const steps0: IterationSteps<T> = [...steps, stepResult.result];
 
   return stepResult.isDone
-    ? iterationSteps
-    : iterateUntilDone(fn, stepResult.result, iterationSteps, depth - 1);
+    ? steps0
+    : iterationSteps(fn, stepResult.result, steps0, depth - 1);
 }
 
-export function iteration<T>(fn: (x: T) => IterationStepResult<T>): (x: T) => IterationSteps<T> {
-  return (x: T) => iterateUntilDone(fn, x, [], 10000);
+// Creates a new function that runs fn to a max depth and returns an object containing the
+// result of each step, until iteration has completed.
+export function iteration<T>(fn: (x: T) => IterationStepResult<T>, depth = 10000): (x: T) => IterationResult<T> {
+  return (x: T) => {
+    const steps: IterationSteps<T> = iterationSteps(fn, x, [], depth);
+    const result = steps[steps.length - 1];
+
+    const iterationResult: IterationResult<T> = {steps, result};
+    return iterationResult;
+  }
 }
--- src/algorithm/sort/bubbleSort.test.ts
+++ src/algorithm/sort/bubbleSort.test.ts
@@ -5,40 +5,40 @@ describe('Bubble Sort', () => {
   test('Sorted array', () => {
     const input = [1, 2, 3, 6, 7, 8];
     const expected = [1, 2, 3, 6, 7, 8];
-    const result = bubbleSort(input);
+    const result = bubbleSort(input).result;
 
-    expect(result[result.length - 1]).toEqual(expected);
+    expect(result).toEqual(expected);
   });
 
   test('Unsorted array', () => {
     const input = [9, 2, 3, 7, 1];
     const expected = [1, 2, 3, 7, 9];
-    const result = bubbleSort(input);
+    const result = bubbleSort(input).result;
 
-    expect(result[result.length - 1]).toEqual(expected);
+    expect(result).toEqual(expected);
   });
 
   test('Single element array', () => {
     const input = [5];
     const expected = [5];
-    const result = bubbleSort(input);
+    const result = bubbleSort(input).result;
 
-    expect(result[result.length - 1]).toEqual(expected);
+    expect(result).toEqual(expected);
   });
 
   test('Empty array', () => {
     const input: number[] = [];
     const expected: number[] = [];
-    const result = bubbleSort(input);
+    const result = bubbleSort(input).result;
 
-    expect(result[result.length - 1]).toEqual(expected);
+    expect(result).toEqual(expected);
   });
 
   test('Array with duped values', () => {
     const input: number[] = [-1, -5, 0, 2, 2, -5, -1, 9, 0];
     const expected: number[] = [-5, -5, -1, -1, 0, 0, 2, 2, 9];
-    const result = bubbleSort(input);
+    const result = bubbleSort(input).result;
 
-    expect(result[result.length - 1]).toEqual(expected);
+    expect(result).toEqual(expected);
   });
 })
