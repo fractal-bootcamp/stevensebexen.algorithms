4925ddbec81d02a82667670716f671c75bfbacd6
Steven <ssebexen@gmail.com>
2024-06-24 10:58:55 -0400

    Ellipsis suggested lowering default recursion depth.

--- src/algorithm/iteration.ts
+++ src/algorithm/iteration.ts
@@ -1,5 +1,5 @@
 // Creates an array of intermediate results from running fn on inputValue.
-export function iterationSteps<T>(fn: (x: T) => IterationStepResult<T>, inputValue: T, steps: IterationSteps<T> = [], depth = 10000): IterationSteps<T> {
+export function iterationSteps<T>(fn: (x: T) => IterationStepResult<T>, inputValue: T, steps: IterationSteps<T> = [], depth = 1000): IterationSteps<T> {
   if (depth <= 0) throw new Error('Recursion depth max reached.');
 
   const stepResult: IterationStepResult<T> = fn(inputValue);
@@ -12,7 +12,7 @@ export function iterationSteps<T>(fn: (x: T) => IterationStepResult<T>, inputVal
 
 // Creates a new function that runs fn to a max depth and returns an object containing the
 // result of each step, until iteration has completed.
-export function iteration<T>(fn: (x: T) => IterationStepResult<T>, depth = 10000): (x: T) => IterationResult<T> {
+export function iteration<T>(fn: (x: T) => IterationStepResult<T>, depth = 1000): (x: T) => IterationResult<T> {
   return (x: T) => {
     const steps: IterationSteps<T> = iterationSteps(fn, x, [], depth);
     const result = steps[steps.length - 1];
